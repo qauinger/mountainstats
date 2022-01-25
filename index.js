0012dfa0f196104fd48124b01d245307186a00a0
qauinger <Qaauinger@icloud.com>
2022-01-25 00:00:52 -0500

    Full site restructure and branding.

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
     const response = await fetch('https://api.github.com/repos/qauinger/mountainstats/contents/mountains');
     const data = await response.json();
     console.log(data);
-    Object.keys(data).forEach(async function(k) {
-        var mtn = data[k];
-        var id = data[k]['name'].slice(0, -5);
+    for (let i = 0; i < data.length; i++) {
+        var mtn = data[i];
+        var id = data[i]['name'];
+        if(!id.endsWith('.json'))
+            break;
+        id = id.slice(0, -5);
         const r = await fetch(mtn['download_url']);
         const d = await r.json();
         var name = d['name'];
         var node = document.createElement('li');
-        node.className = 'mtn';
+        node.classList.add('mtn');
         var link = document.createElement('a');
-        link.setAttribute('href', 'render.html?m=' + id);
+        link.setAttribute('href', 'render?' + id);
         link.innerHTML = name + ' (' + d['region'] + ')';
         node.appendChild(link);
-        document.getElementById('mountains').appendChild(node);
-    })
+        document.getElementById('mtn-list').appendChild(node);
+    }
 })()
--- render.js
+++ render.js
@@ -1,51 +1,37 @@
 (async () => {
-    fetch('https://raw.githubusercontent.com/qauinger/mountainstats/master/mountains/' + getParams()['m'] + '.json')
+    fetch('https://raw.githubusercontent.com/qauinger/mountainstats/master/mountains/' + getMtn() + '.json')
     .then(response => {
         if (response.ok) {
-            return response.json()
+            return response.json();
         } else if(response.status === 404) {
-            return Promise.reject('error 404')
+            return Promise.reject('error 404');
         } else {
-            return Promise.reject('some other error: ' + response.status)
+            return Promise.reject('some other error: ' + response.status);
         }
     })
     .then(data => loadData(data))
     .catch(error => invalid());
 })()
 
-function getParams() {
+function getMtn() {
     var idx = document.URL.indexOf('?');
-    var params = new Array();
+    var id = new String();
     if (idx != -1) {
-        var pairs = document.URL.substring(idx + 1, document.URL.length).split('&');
-        for (var i = 0; i < pairs.length; i++) {
-            var nameVal = pairs[i].split('=');
-            params[nameVal[0]] = nameVal[1];
-        }
+        id = document.URL.substring(idx + 1, document.URL.length);
     }
-    return params;
+    return id;
 }
 
 function loadData(data) {
-    var title = document.createElement('a');
-    title.setAttribute('href', data['webpage'])
-    title.setAttribute('style', 'text-decoration: none;')
-    title.setAttribute('target', '_blank')
-    var header = document.createElement('h1');
-    header.innerHTML = data['name'];
-    title.appendChild(header);
-
-    document.body.appendChild(title)
-    //document.getElementById('header').innerHTML = 'Loading ' + data['name'] + '...';
-    document.getElementById('title').innerHTML = 'Mountain Stats' + data['name'];
+    document.title = 'MountainStats | ' + data['name'];
+    document.getElementById('title').innerHTML = data['name'];
+    document.getElementById('webpage').setAttribute('href', data['webpage']);
+    document.getElementById('webpage').setAttribute('target', '_blank');
+    document.getElementById('webpage').classList.add('link');
+    document.getElementById('webpage').innerHTML = data['webpage'];
 }
 
 function invalid() {
-    document.getElementById('title').innerHTML = 'Mountain Stats | Invalid!';
-    document.getElementById('header').innerHTML = 'Unknown mountain.';
-    var ret = document.createElement('a');
-    ret.setAttribute('href', 'index.html');
-    ret.setAttribute('style', 'text-decoration: none;');
-    ret.innerHTML = '<- Return';
-    document.body.appendChild(ret);
+    document.title = 'Mountain Stats';
+    document.getElementById('title').innerHTML = 'Invalid mountain.';
 }
